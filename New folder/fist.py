a6af5feca1264bef4c4d1990f356958b8c062a50
harshaprabha874n <nharshaprabha@gmail.com>
2024-08-10 18:58:11 +0530

    added fist.py

--- New folder/fist.py
+++ New folder/fist.py
@@ -1,4 +1,9 @@
-a[5]={10,20,30,40,50}
-if(a%2==0):
-    b=a
-    b.length()
+#count the number of even numbers in array
+arr=[10,43,55,86]
+count=0
+for i in arr:
+    if i % 2 == 0:
+        count+=1
+print(count)
+
+
