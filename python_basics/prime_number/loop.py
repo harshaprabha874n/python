2ada71da39ce99c16978672aeb50ac45f2dc9b34
harshaprabha874n <nharshaprabha@gmail.com>
2024-09-25 22:59:32 +0530

    add

--- python_basics/prime_number/loop.py
+++ python_basics/prime_number/loop.py
@@ -1,7 +1,10 @@
-num=2
+num=int(input("Enter the number:"))
+sump=0
 for i in range(2,num):
     if(num%i==0):
-        print("not")
-        break
-    else:
-        print("prime")
+        sump=sump+i
+        if(sump==num):
+          print("Prime")
+          break
+        else:
+           print("Not")
